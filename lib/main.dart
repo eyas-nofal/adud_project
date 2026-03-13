fc30adae1142741a666cea57c571928149315d15
eyas-nofal <eyasnofalb@gmail.com>
2026-03-13 22:17:34 +0300

    removed hello world

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      
       ),
     );
   }
