e50cd1222dfd213f2853489d066c47788eb0f018
eyas-nofal <eyasnofalb@gmail.com>
2026-03-13 23:34:04 +0300

    Add full app translations and localization setup

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -9,5 +9,267 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get welcome => 'Welcome';
+  String get appName => 'Adud';
+
+  @override
+  String get appTagline => 'Support, Access, Empowerment';
+
+  @override
+  String get appDescription =>
+      'A social platform connecting people with disabilities with nearby volunteers for support and assistance';
+
+  @override
+  String get togetherWeMakeDifference => 'Together we make a difference';
+
+  @override
+  String get createAccount => 'Create New Account';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get changeLanguage => 'Change Language';
+
+  @override
+  String get selectUserType => 'Select User Type';
+
+  @override
+  String get selectUserTypeSub => 'Determine how you want to use the app';
+
+  @override
+  String get disabilityUser => 'Person with Disability';
+
+  @override
+  String get disabilityUserDesc => 'I need help and support from volunteers';
+
+  @override
+  String get volunteer => 'Volunteer';
+
+  @override
+  String get volunteerDesc => 'I want to provide help and support to others';
+
+  @override
+  String get canChangeTypeLater =>
+      'Note: You can change the account type later from settings';
+
+  @override
+  String get welcomeBack => 'Welcome back';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forgotPassword => 'Forgot Password?';
+
+  @override
+  String get loginWithGoogle => 'Login with Google';
+
+  @override
+  String get loginWithFacebook => 'Login with Facebook';
+
+  @override
+  String get dontHaveAccount => 'Don\'t have an account? Create new account';
+
+  @override
+  String get loginAs => 'Login as';
+
+  @override
+  String get joinUsStartJourney => 'Join us and start your journey';
+
+  @override
+  String get firstName => 'First Name';
+
+  @override
+  String get confirmPassword => 'Confirm Password';
+
+  @override
+  String get agreeToTerms =>
+      'I agree to the Terms & Conditions and Privacy Policy';
+
+  @override
+  String get passwordMismatch => 'Passwords do not match';
+
+  @override
+  String get alreadyHaveAccount => 'Already have an account? Login';
+
+  @override
+  String welcomeUser(Object name) {
+    return 'Welcome, $name';
+  }
+
+  @override
+  String get howCanWeHelp => 'How can we help you today?';
+
+  @override
+  String get nearbyAccessiblePlaces => 'Accessible places near you';
+
+  @override
+  String get requestHelp => 'Request Help';
+
+  @override
+  String get requestHelpDesc => 'Immediate or scheduled';
+
+  @override
+  String get activeRequests => 'Active and upcoming requests';
+
+  @override
+  String get noActiveRequests => 'No active requests currently';
+
+  @override
+  String get pressRequestHelp =>
+      'Press \"Request Help\" to create a new request';
+
+  @override
+  String get reportInaccessiblePlace => 'Report an inaccessible place';
+
+  @override
+  String get improveAccessibility => 'Help us improve accessibility';
+
+  @override
+  String get transportation => 'Transportation';
+
+  @override
+  String get escort => 'Escorting';
+
+  @override
+  String get buyingGroceries => 'Buying Groceries';
+
+  @override
+  String get universityHelp => 'University Help';
+
+  @override
+  String get live => 'Live';
+
+  @override
+  String get upcoming => 'Upcoming';
+
+  @override
+  String get call => 'Call';
+
+  @override
+  String get details => 'Details';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get navHome => 'Home';
+
+  @override
+  String get navDonate => 'Donate';
+
+  @override
+  String get navWorkshops => 'Workshops';
+
+  @override
+  String get navProfile => 'Profile';
+
+  @override
+  String get navHonorRoll => 'Honor Roll';
+
+  @override
+  String get thanksForGiving => 'Thank you for your continuous giving';
+
+  @override
+  String get rewardPoints => 'Reward Points';
+
+  @override
+  String get nearbyRequests => 'Nearby Requests';
+
+  @override
+  String get acceptRequest => 'Accept Request';
+
+  @override
+  String get rewardOffers => 'Reward Offers';
+
+  @override
+  String get redeem => 'Redeem';
+
+  @override
+  String get completedHelp => 'Completed Help';
+
+  @override
+  String get rating => 'Rating';
+
+  @override
+  String get thisMonth => 'This Month';
+
+  @override
+  String get fillDetailsFindVolunteer =>
+      'Fill the details and we will find a nearby volunteer';
+
+  @override
+  String get helpType => 'Type of help required';
+
+  @override
+  String get requestNow => 'Now';
+
+  @override
+  String get requestLater => 'Later';
+
+  @override
+  String get selectDate => 'Select Date';
+
+  @override
+  String get selectTime => 'Select Time';
+
+  @override
+  String get expectedDuration => 'Expected duration (Hours)';
+
+  @override
+  String get payVolunteer => 'Do you want to pay the volunteer?';
+
+  @override
+  String get amountJOD => 'Amount in JOD';
+
+  @override
+  String get additionalNotes => 'Additional notes (Optional)';
+
+  @override
+  String get sendRequest => 'Send Request';
+
+  @override
+  String get activeVolunteer => 'Active Volunteer';
+
+  @override
+  String get user => 'User';
+
+  @override
+  String get completedActivity => 'Completed Activity';
+
+  @override
+  String get providedHelp => 'Help Provided';
+
+  @override
+  String get receivedHelp => 'Help Received';
+
+  @override
+  String get contributionHours => 'Contribution Hours';
+
+  @override
+  String get earnedPoints => 'Earned Points';
+
+  @override
+  String get honorRollDesc => 'View your achievements and rank';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get notifications => 'Notifications';
+
+  @override
+  String get privacyPolicy => 'Privacy Policy';
+
+  @override
+  String get termsAndConditions => 'Terms and Conditions';
+
+  @override
+  String get helpSupport => 'Help and Support';
+
+  @override
+  String get logout => 'Logout';
 }
