84618cdd73db05279f3a06950bd318271b15f963
eyas-nofal <eyasnofalb@gmail.com>
2026-03-13 23:26:48 +0300

    added languages

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,45 @@
+import 'package:adud_project/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+
+  static void setLocale(BuildContext context, Locale newLocale) {
+    _MainAppState? state = context.findAncestorStateOfType<_MainAppState>();
+    state?.changeLanguage(newLocale);
+  }
+}
+
+class _MainAppState extends State<MainApp> {
+  Locale _locale = const Locale('ar');
+  void changeLanguage(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-      
-      ),
-    );
+    return MaterialApp(
+      locale: _locale,
+      debugShowCheckedModeBanner: false,
+    localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('ar'), // Arabic
+      ],
+      home: const Scaffold(),);
   }
 }
