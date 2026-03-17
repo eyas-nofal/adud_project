6cda1b987468a700b545febb0716ce69eda3ee6d
OmarDandis <omar.dandis4@gmail.com>
2026-03-17 16:14:12 +0300

    ceate

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,10 @@
+import 'package:adud_project/l10n/app_localizations.dart';
+import 'package:adud_project/screens/core/constants/app_colors.dart';
 import 'package:adud_project/screens/core/widgets/custom_button.dart';
 import 'package:adud_project/screens/core/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,11 +17,24 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 400,),
-          
-        ],
+      backgroundColor: AppColors.primary50,
+
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              AppLocalizations.of(context)!.login,
+              /*style: GoogleFonts.cairo(
+                fontSize: 40.sp,
+                fontWeight: FontWeight.w700,
+                color: AppColors.primary700,
+              ),*/
+            ),
+          ],
+        ),
       ),
     );
   }
