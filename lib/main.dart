f8c1cf9d63b70063e9320827d7c66e3208a7f88f
eyas-nofal <eyasnofalb@gmail.com>
2026-03-18 22:27:57 +0300

    edited on the main to add languageProvider notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app.dart'; 
-import 'provider/language_provider.dart';
+import 'package:adud_project/providers/language_provider.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   
-  
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => LanguageProvider(), 
+      child: const AppRoot(),
+    ),
+  );
 }
