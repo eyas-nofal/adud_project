9d6aafc3db8ac71ee86ef63c8d88cc32024e9b9f
eyas-nofal <eyasnofalb@gmail.com>
2026-03-18 22:26:57 +0300

    moved languageProvider to lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app.dart'; 
+import 'provider/language_provider.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   
