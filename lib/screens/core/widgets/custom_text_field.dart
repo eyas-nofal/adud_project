557d6e425c8a62135b81e929f4a5720f1935a191
eyas-nofal <eyasnofalb@gmail.com>
2026-03-18 23:46:42 +0300

    eddited on custom_text_field to be responsive

--- lib/screens/core/widgets/custom_text_field.dart
+++ lib/screens/core/widgets/custom_text_field.dart
@@ -1,41 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
-final decorationForTextField = InputDecoration(
-  filled: true,
-  fillColor: Colors.grey[50],   
-  
-  hintStyle: TextStyle(color: Colors.grey[400], fontSize: 14),
-  
-  enabledBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(16), 
-    borderSide: BorderSide(color: Colors.grey[200]!, width: 1.5),
-  ),
-  
-  focusedBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(16),
-    borderSide: const BorderSide(color: Colors.blueAccent, width: 2),
-  ),
-  
-  errorBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(16),
-    borderSide: const BorderSide(color: Colors.redAccent, width: 1.5),
-  ),
-  
-  focusedErrorBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(16),
-    borderSide: const BorderSide(color: Colors.redAccent, width: 2),
-  ),
-  
-  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
-  
-);
-
-/*
-TextField(
-  decoration: decorationForTextField.copyWith(
-    hintText: "user name", 
-    prefixIcon: Icon(Icons.person), 
-  ),
-)
-*/
+class AppInputStyles {
+  static InputDecoration decorationForTextField({
+    required String hintText,
+    IconData? prefixIcon,
+    Widget? suffixIcon,
+  }) {
+    return InputDecoration(
+      hintText: hintText,
+      prefixIcon: prefixIcon != null ? Icon(prefixIcon, color: Colors.grey[400]) : null,
+      suffixIcon: suffixIcon,
+      filled: true,
+      fillColor: Colors.grey[50],
+      hintStyle: TextStyle(color: Colors.grey[400], fontSize: 14.sp),
+      
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16.r),
+        borderSide: BorderSide(color: Colors.grey[200]!, width: 1.5),
+      ),
+      
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16.r),
+        borderSide: const BorderSide(color: Colors.blueAccent, width: 2),
+      ),
+      
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16.r),
+        borderSide: const BorderSide(color: Colors.redAccent, width: 1.5),
+      ),
+      
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16.r),
+        borderSide: const BorderSide(color: Colors.redAccent, width: 2),
+      ),
+      
+      contentPadding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 18.h),
+    );
+  }
+}
