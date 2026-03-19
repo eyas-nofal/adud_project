eeacd1e4548048044d42592d9c402422818060a2
eyas-nofal <eyasnofalb@gmail.com>
2026-03-19 23:25:16 +0300

    edited on the login text button in create new account page

--- lib/screens/auth/create_new_account.dart
+++ lib/screens/auth/create_new_account.dart
@@ -1,5 +1,6 @@
 import 'package:adud_project/l10n/app_localizations.dart';
 import 'package:adud_project/screens/auth/login_screen.dart';
+import 'package:adud_project/screens/auth/register_screen.dart';
 import 'package:adud_project/screens/core/constants/app_colors.dart';
 import 'package:adud_project/screens/core/widgets/custom_button.dart';
 import 'package:adud_project/screens/core/widgets/custom_text_field.dart';
@@ -141,29 +142,41 @@ class _CreateNewAccountState extends State<CreateNewAccount> {
                 backgroundColor: Colors.blue,
                 textColor: Colors.white,
                 text: AppLocalizations.of(context)!.createAccount,
-                onPressed: () {},
+                onPressed: () {
+                    Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterScreen(),
+                          ),
+                        );
+                },
               ),
 
               SizedBox(height: 20.h),
 
               Center(
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(AppLocalizations.of(context)!.alreadyHaveAccount),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        AppLocalizations.of(context)!.login,
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    );
-                  },
-                  child: Text(
-                    AppLocalizations.of(context)!.alreadyHaveAccount,
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.bold,
                     ),
-                  ),
+                  ],
                 ),
               ),
             ],
