b8ed5072812503d0d86d7a46f6df34366d8db643
OmarDandis <omar.dandis4@gmail.com>
2026-03-19 13:24:16 +0300

    Updated login screen

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,10 +1,10 @@
 import 'package:adud_project/l10n/app_localizations.dart';
+import 'package:adud_project/screens/auth/create_new_account.dart';
 import 'package:adud_project/screens/core/constants/app_colors.dart';
 import 'package:adud_project/screens/core/widgets/custom_button.dart';
 import 'package:adud_project/screens/core/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -17,23 +17,113 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primary50,
+      backgroundColor: AppColors.primary100,
 
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              AppLocalizations.of(context)!.login,
-              /*style: GoogleFonts.cairo(
-                fontSize: 40.sp,
-                fontWeight: FontWeight.w700,
-                color: AppColors.primary700,
-              ),*/
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 65.h),
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      AppLocalizations.of(context)!.login,
+                      style: TextStyle(
+                        fontSize: 25.sp,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(height: 12.h),
+                    Text(
+                      AppLocalizations.of(context)!.welcomeBack,
+                      style: TextStyle(fontSize: 15.sp, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 60),
+              Text(
+                AppLocalizations.of(context)!.email,
+                style: TextStyle(fontSize: 18.sp, color: Colors.black),
+              ),
+              SizedBox(height: 10.h),
+              TextFormField(
+                decoration: AppInputStyles.decorationForTextField(
+                  hintText: "example@gmail.com",
+                  prefixIcon: Icons.email_outlined,
+                ),
+              ),
+              SizedBox(height: 20.h),
+
+              Text(
+                AppLocalizations.of(context)!.password,
+                style: TextStyle(fontSize: 18.sp, color: Colors.black),
+              ),
+              SizedBox(height: 10.h),
+              TextFormField(
+                obscureText: true,
+                decoration: AppInputStyles.decorationForTextField(
+                  hintText: "*********",
+                  prefixIcon: Icons.lock,
+                  suffixIcon: Icon(Icons.visibility),
+                ),
+              ),
+              SizedBox(height: 15.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(AppLocalizations.of(context)!.forgotPassword),
+                  ),
+                ],
+              ),
+              CustomButton(
+                width: double.infinity,
+                height: 50.h,
+                backgroundColor: Colors.blue,
+                textColor: Colors.white,
+                text: AppLocalizations.of(context)!.login,
+                onPressed: () {},
+              ),
+              SizedBox(height: 15,),
+              SizedBox(
+                height: 50,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(thickness: 0.6, color: Colors.black),
+                    ),
+                    Text("OR"),
+                    Expanded(
+                      child: Divider(thickness: 0.6, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 200.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("ليس لديك حساب؟"),
+                  SizedBox(width: 2.w),
+                  TextButton(onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CreateNewAccount(),
+                      ),
+                    );
+                  }, child: Text("انشاء حساب جديد")),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
