36643d04760fb16830a43f25422a192c8027fc3e
eyas-nofal <eyasnofalb@gmail.com>
2026-05-04 22:01:21 +0300

    added app loader widget

---
+++ lib/screens/core/widgets/custom_app_loader.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppLoader extends StatelessWidget {
+  final double size;
+  final Color color;
+
+  const AppLoader({
+    super.key, 
+    this.size = 50.0, 
+    this.color = Colors.blue, // يمكنك وضع لون تطبيقك الأساسي هنا
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SpinKitDualRing(
+        color: color,
+        size: size.w,
+        lineWidth: 3.0, // سُمك الحلقة
+      ),
+    );
+  }
+}
