7206e63cb77a20b6173135ec5db18654e252fc0d
OmarDandis <omar.dandis4@gmail.com>
2026-05-05 20:16:37 +0300

    start in voulunteer home screen and edit size text 2 from custom app header to 15

--- lib/screens/home/volunteer_home_screen.dart
+++ lib/screens/home/volunteer_home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:adud_project/l10n/app_localizations.dart';
+import 'package:adud_project/screens/core/constants/app_colors.dart';
+import 'package:adud_project/screens/core/widgets/custom_app_header.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class VolunteerHomeScreen extends StatefulWidget {
   const VolunteerHomeScreen({super.key});
@@ -12,8 +15,31 @@ class _VolunteerHomeScreenState extends State<VolunteerHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Volunteer Home Screen"),
+      backgroundColor: AppColors.primary100,
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: CustomAppHeader(
+              title: AppLocalizations.of(context)!
+                  .welcomeUser("Ahmad"),
+              subTitle: AppLocalizations.of(context)!
+                  .thanksForGiving,
+              notifications: 5,
+              messages: 3,
+            ),
+          ),
+          Expanded(
+            //child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(20.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [],
+                ),
+              ),
+            ),
+        //  ),
+        ],
       ),
     );
   }
